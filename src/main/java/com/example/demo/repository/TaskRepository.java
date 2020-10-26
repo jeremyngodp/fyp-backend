ebc3514752862877ab53a274dd1d9a271125e880
Jeremy Ngo <44996654+jeremyngodp@users.noreply.github.com>
2020-10-26 23:46:08 +0800

    Add method to find Task by Project persistence (with JPQL) and by Project_id (with native SQL)

--- src/main/java/com/example/demo/repository/TaskRepository.java
+++ src/main/java/com/example/demo/repository/TaskRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+import com.example.demo.persistences.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.persistences.Task;
@@ -12,6 +13,9 @@ public interface TaskRepository extends JpaRepository<Task, Integer> {
     @Query("SELECT t FROM Task t WHERE t.student_id = ?1")
     List<Task> findbystudentID (int student_id);
 
-    @Query("SELECT t FROM Task t WHERE t.project_id = ?1")
+//    @Query("SELECT t FROM Task t WHERE t.project_id = ?1")
+    List<Task> findByProject(Project project);
+
+    @Query(value = "SELECT * FROM task WHERE project_id = ?1", nativeQuery = true)
     List<Task> findByProjectId(int project_id);
 }
