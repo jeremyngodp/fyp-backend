cab5cb5ca3ed079ef154d5b129ec1c00870d2073
Jeremy Ngo <44996654+jeremyngodp@users.noreply.github.com>
2021-04-09 15:04:24 +0800

    include Put Mapping for reset and change pwd, include Delete mapping to delete user

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.example.demo.assemblers.UserModelAssembler;
@@ -45,20 +46,21 @@ public class UserController {
 	private final ProjectService projectService;
 
 	@Autowired
-	public UserController (UserService userService,
-						   ProjectService projectService,
-						   UserModelAssembler userModelAssembler,
-						   AuthenticationManager authenticationManager,
-						   JwtTokenUtil jwtTokenUtil) {
+	public UserController(UserService userService,
+						  ProjectService projectService,
+						  UserModelAssembler userModelAssembler,
+						  AuthenticationManager authenticationManager,
+						  JwtTokenUtil jwtTokenUtil) {
 		super();
 		this.userAssembler = userModelAssembler;
 		this.userService = userService;
 		this.authenticationManager = authenticationManager;
 		this.jwtTokenUtil = jwtTokenUtil;
-		this.projectService= projectService;
+		this.projectService = projectService;
 	}
-    @PostMapping(value = "/add", consumes = "application/json", produces = "application/json")
-	public ResponseEntity<?> addUser (@RequestBody UserDTO userdto) {
+
+	@PostMapping(value = "/add", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<?> addUser(@RequestBody UserDTO userdto) {
 		User newUser = new User();
 		newUser.setUsername(userdto.getUsername());
 		newUser.setPassword(userdto.getPassword());
@@ -84,39 +86,65 @@ public class UserController {
 		final String token = jwtTokenUtil.generateToken(userDetails);
 		User user = userService.findUserByUsername(authenticationRequest.getUsername());
 		List<Project> projects = new ArrayList<>();
-		if(user.isIs_staff()) {
+		if (user.isIs_staff()) {
 			projects = projectService.findBySupervisorID(user.getId());
-		}
-		else if (!user.isIs_staff()) {
+		} else if (!user.isIs_staff()) {
 			projects = projectService.findbyStudentID(user.getId());
 		}
 		return ResponseEntity.ok(new JwtResponse(token, user, projects));
 	}
 
-    private void authenticate(String username, String password) throws Exception {
-        try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-        } catch (DisabledException e) {
-            throw new Exception("USER_DISABLED", e);
-        } catch (BadCredentialsException e) {
-            throw new Exception("INVALID_CREDENTIALS", e);
-        }
-    }
+	private void authenticate(String username, String password) throws Exception {
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+		} catch (DisabledException e) {
+			throw new Exception("USER_DISABLED", e);
+		} catch (BadCredentialsException e) {
+			throw new Exception("INVALID_CREDENTIALS", e);
+		}
+	}
 
-	@GetMapping(value="/all")
+	@GetMapping(value = "/all")
 	public CollectionModel<EntityModel<User>> getAllUsers() {
 		List<EntityModel<User>> users = userService.findAllUser()
-										.stream()
-										.map(userAssembler::toModel)
-										.collect(Collectors.toList());
+				.stream()
+				.map(userAssembler::toModel)
+				.collect(Collectors.toList());
 
 		return CollectionModel.of(users, linkTo(methodOn(UserController.class).getAllUsers()).withSelfRel());
 	}
 
-	@GetMapping(value="/{username}")
+	@GetMapping(value = "/{username}")
 	public EntityModel<User> getUserByUsername(@PathVariable String username) {
 		User user = userService.findUserByUsername(username);
 
 		return userAssembler.toModel(user);
+	}
+
+	@PutMapping(value = "/{username}", consumes = "application/json", produces = "application/json")
+	public EntityModel<User> updateUser(@PathVariable String username) {
+		User user = userService.findUserByUsername(username);
+		user.setIs_admin(!user.isIs_admin());
+		EntityModel<User> updatedUser = userAssembler.toModel(userService.addUser(user));
+
+		return updatedUser;
+	}
+
+	@PutMapping(value = "resetpwd/{username}", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<?> resetpwd(@PathVariable String username, @RequestBody String password) {
+		userService.resetPassword(username);
+		return ResponseEntity.ok(username);
+	}
+
+	@PutMapping(value = "changepwd/{username}", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<?> changepwd(@PathVariable String username, @RequestBody Map<String, String> body) {
+		userService.changePassword(username, body.get("password"));
+		return ResponseEntity.ok(username);
+	}
+
+	@DeleteMapping (value="remove/{username}")
+	public ResponseEntity<?> remove(@PathVariable String username) {
+		userService.remove(username);
+		return ResponseEntity.ok(username);
 	}
 }
